ff560577bedc6ce93b43a481994e449756ad5a68
Bonnee <bonora.matteo@gmail.com>
2019-05-27 14:27:15 +0200

    Update error management

--- niucharge.js
+++ niucharge.js
@@ -9,7 +9,7 @@ const storage = require('node-persist');
 const scooter = config.get('scooter');
 const plug = config.get('plug');
 
-storage.init( /* options ... */ );
+storage.init();
 
 var limit = 90;
 
@@ -32,27 +32,44 @@ app.use(bodyParser.urlencoded({
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
+	handlePromises().then(data => {
+		if (data[0] instanceof Error) {
+			res.statusCode(500).send();
+			return;
+		}
 
-	updateState().then((data) => {
-		device.get({
-			schema: true
-		}).then(plugData => {
-			plugData = plugData.dps
+		let plugData = {
+			state: -1,
+			current: -1,
+			power: -1,
+			volt: -1,
+		};
 
-			res.render('index', {
-				data: data,
-				soc: getSOC(data),
-				plugData: {
-					state: plugData['1'],
-					current: (plugData['4'] / 1000).toFixed(2),
-					power: Math.round(plugData['5'] / 10),
-					volt: Math.round(plugData['6'] / 10)
-				},
-				chargeLimit: limit
-			});
+		if (!(data[1] instanceof Error)) {
+			plugData = {
+				state: data[1].dps['1'],
+				current: (data[1].dps['4'] / 1000).toFixed(2),
+				power: Math.round(data[1].dps['5'] / 10),
+				volt: Math.round(data[1].dps['6'] / 10)
+			}
+		}
+
+		res.render('index', {
+			data: data[0],
+			soc: getSOC(data[0]),
+			plugData,
+			chargeLimit: limit
 		});
 	});
 });
+
+async function handlePromises() {
+	let promises = [updateState(), device.get({
+		schema: true
+	})];
+
+	return await Promise.all(promises.map(p => p.catch(e => e)));
+}
 
 app.post('/charging', (req, res) => {
 	device.set({
@@ -137,39 +154,39 @@ async function setLimit(newLimit) {
 
 }
 
-function setPlug(turn_on) {
-	device.set({
+async function setPlug(turn_on) {
+	await device.set({
 		set: turn_on
 	});
 }
 
 function connectPlug() {
-	let stateHasChanged = false;
-
 	// Find device on network
 	device.find().then(() => {
 		// Connect to device
 		device.connect();
-
+	}).catch((e) => {
+		console.error("Error finding plug, retrying", e);
+		connectPlug();
 	});
-
-	// Add event listeners
-	device.on('connected', () => {
-		console.log('Connected to plug!');
-	});
-
-	device.on('disconnected', () => {
-		console.log('Disconnected from plug.');
-	});
-
-	device.on('error', error => {
-		console.log('Elug error!', error);
-	});
-
 }
 
+// Add event listeners
+device.on('connected', () => {
+	console.log('Connected to plug!');
+});
+
+device.on('disconnected', () => {
+	console.log('Disconnected from plug. Reconnecting...');
+	connectPlug();
+});
+
+device.on('error', error => {
+	console.log('Elug error!', error);
+});
+
 function updateState() {
-	return new Promise((accept, reject) => {
+	return new Promise((resolve, reject) => {
 		var post_options = {
 			host: 'app-api-fk.niu.com',
 			path: '/v3/motor_data/index_info?sn=' + scooter.sn,
@@ -185,7 +202,7 @@ function updateState() {
 			res.setEncoding('utf8');
 			res.on('data', function (chunk) {
 
-				accept(JSON.parse(chunk).data);
+				resolve(JSON.parse(chunk).data);
 
 			});
 		});
