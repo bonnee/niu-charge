d36c84a9577576ab2f7806882bb5268e9ab48b67
Bonnee <bonora.matteo@gmail.com>
2019-06-16 12:51:46 +0200

    Add basic support for single battery scooters

--- src/niu.js
+++ src/niu.js
@@ -11,23 +11,53 @@ module.exports = class Niu {
 	}
 
 	static computeSOC(data) {
-		let battery = [data.batteries.compartmentA, data.batteries.compartmentB];
 		let soc = 0;
 
-		if (battery[0].isConnected) {
-			soc += battery[0].batteryCharging
-		}
-		if (battery[1].isConnected) {
-			soc += battery[1].batteryCharging
-		}
+		if (data.batteries.compartmentB instanceof Object) {
+			let battery = [data.batteries.compartmentA, data.batteries.compartmentB];
 
-		if (battery[0].isConnected && battery[1].isConnected) {
-			return soc / 2;
-		}
+			if (battery[0].isConnected) {
+				soc += battery[0].batteryCharging
+			}
+			if (battery[1].isConnected) {
+				soc += battery[1].batteryCharging
+			}
 
+			if (battery[0].isConnected && battery[1].isConnected) {
+				return soc / 2;
+			}
+		} else {
+			if (data.batteries.compartmentA.isConnected) {
+				soc = data.batteries.compartmentA.batteryCharging;
+			}
+		}
 		return soc;
 	}
 
+	getImage() {
+		let self = this;
+		return new Promise((resolve, reject) => {
+			var post_options = {
+				host: 'app-api-fk.niu.com',
+				path: '/motoinfo/list',
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'token': this.token
+				}
+			};
+
+			// Set up the request
+			var post_req = https.request(post_options, res => {
+				res.setEncoding('utf8');
+				res.on('data', function (chunk) {
+					resolve(JSON.parse(chunk).data[0].scootorImg);
+				});
+			});
+
+			post_req.end();
+		});
+	}
 
 	get() {
 		return this._data;
